9c48cdc1b16ee8fed56486037ddfa6995d7dbb92
Benedikt Þorri Þórarinsson <benedikt@Benedikts-MacBook-Pro.local>
2017-10-24 17:10:29 +0000

    build.gradle file modified to accept input in console. Constructor created, private variables declared. getStatus function created to get index of array and unit test added in test class.

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -1,7 +1,25 @@
 package is.ru.tictactoe;
 
 public class TicTacToe{
-	public TicTacToe(){
 
+	private static boolean gameOver = false;
+	private static boolean whoseTurn = true;
+	private static char [][] gameArr;
+
+	public TicTacToe(){
+		gameArr = new char [3][3];
+		gameArr[0][0] = '1';
+		gameArr[0][1] = '2';
+		gameArr[0][2] = '3';
+		gameArr[1][0] = '4';
+		gameArr[1][1] = '5';
+		gameArr[1][2] = '6';
+		gameArr[2][0] = '7';
+		gameArr[2][1] = '8';
+		gameArr[2][2] = '9';
+	}
+	
+	public static char getStatus(int row, int col){
+		return gameArr[row][col];
 	}
 }
--- src/test/java/is/ru/tictactoe/TestTicTacToe.java
+++ src/test/java/is/ru/tictactoe/TestTicTacToe.java
@@ -4,5 +4,14 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TestTicTacToe{
+	@Test
+	public void testArrayIndex(){
+		TicTacToe ttt = new TicTacToe();
+		assertEquals('1', ttt.getStatus(0, 0));
+		assertEquals('2', ttt.getStatus(0, 1));
+		assertEquals('3', ttt.getStatus(0, 2));
+		assertEquals('9', ttt.getStatus(2, 2));
+		assertEquals('5', ttt.getStatus(1, 1));
+	}
 	
 }
